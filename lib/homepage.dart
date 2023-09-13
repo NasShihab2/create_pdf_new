01c146e0218d29c3128ea4c44ad3555fd48c0519
Nas <nas.shihab92@gmail.com>
2023-09-13 20:16:03 +0600

    2ff

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,12 +1,57 @@
-import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:mac_address/mac_address.dart';
+
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    String macAddress = '';
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  String macAddress = '';
+
+  void getMacAddress() async {
+    try {
+      String macAddress = await GetMac.macAddress;
+      print('A MAC Address: $macAddress');
+    } catch (e) {
+      print('Failed to get MAC address: $e');
+    }
+  }
+
+  Future<void> getDeviceInfo() async {
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String macAddress = 'Not available'; // Default value
+
+    try {
+      if (!kIsWeb) {
+        if (Platform.isAndroid) {
+          AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+          macAddress = androidInfo.macAddress ?? 'Not available';
+        } else if (Platform.isIOS) {
+          // On iOS, the MAC address is not available due to privacy restrictions
+          macAddress = 'Not available';
+        }
+      } else {
+        // Handle the case of running on the web (if needed)
+        macAddress = 'Not available for web';
+      }
+    } catch (e) {
+      print('Error getting device info: $e');
+    }
+
+    print('MAC Address: $macAddress');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -16,8 +61,8 @@ class HomePage extends StatelessWidget {
             Text(macAddress),
             Center(
               child: ElevatedButton(
+                onPressed: getMacAddress,
                 child: const Text('Mac Address'),
-                onPressed: () {},
               ),
             ),
           ],
